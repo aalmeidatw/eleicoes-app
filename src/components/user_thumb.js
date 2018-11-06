01dd6fc9ce1e95868476e66514209f77d3789121
Andre Almeida <aalmeida@thoughtworks.com>
2018-11-06 09:55:11 -0200

    WIP - adiciona icone no thumb

---
+++ src/components/user_thumb.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './man_icon.png'
+
+class ThumbUser extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src="man_icon.png" alt="icon" />
+      </div>
+    )
+  }
+}
+export default ThumbUser;
