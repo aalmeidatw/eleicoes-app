98389b017c754338f26f0849773332fd94cf548a
Andre Almeida <aalmeida@thoughtworks.com>
2018-11-07 14:45:38 -0200

    altera path do icone

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import ThumbUser from './components/user_thumb'
+import TextArea from './components/text_input_area'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <ThumbUser />
+        <TextArea />
       </div>
     );
   }
--- src/components/user_thumb.js
+++ src/components/user_thumb.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-import icon from  './man_icon.png'
+import icon from '../man_icon.png'
 
 class ThumbUser extends React.Component {
   render() {
     return (
       <div>
-        <img src={icon} alt="icon" />
+        <img src={icon} alt="icon" height="100" width="100" />
       </div>
     )
   }
 }
+
 export default ThumbUser;
