32f7b8515951c3d907abf6b06900ca156922a344
Andre Almeida <aalmeida@thoughtworks.com>
2018-11-07 14:46:50 -0200

    adiciona text field

---
+++ src/components/text_input_area.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class TextArea extends React.Component {
+  render() {
+    return (
+      <div>
+        <label> Total: </label>
+        <input type="text" placeholder="Votos aqui" ></input>
+      </div>
+    )
+  }
+}
+
+export default TextArea;
