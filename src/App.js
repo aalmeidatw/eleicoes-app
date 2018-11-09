03a422c38800179d0b26073884edbd8cb6eaef4b
Andre Almeida <aalmeida@thoughtworks.com>
2018-11-09 09:22:44 -0200

    cria button component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import ThumbUser from './components/user_thumb'
 import TextArea from './components/text_input_area'
+import Button from './components/button'
 
 import manIcon from './man_icon.png'
 import womanIcon from './woman_icon.png'
@@ -23,6 +24,7 @@ class App extends Component {
       <div className="App">
         <ThumbUser icon={icons.woman} />
         <TextArea {...textAreaOptions} />
+        <Button />
       </div>
     );
   }
